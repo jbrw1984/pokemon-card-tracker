782146e318535f00f4022ffa10225a008e014e67
Avery Tribbett <apatribbett03@gmail.com>
2023-03-01 19:02:43 -0600

    Added infile for nav bar and placed nav in index.tsx

---
+++ src/Components/NavBar/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Nav extends React.Component {
+    render () {
+        return (
+            <nav>Pokemon Heading</nav>
+        );
+    }
+}
+
+export default Nav;
