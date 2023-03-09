f4ab548b54333bed01c49d8abdecd100a558ebd6
Kobe Yang <kobeyang160@gmail.com>
2023-03-08 18:34:34 -0800

    added very simple react-router-dom routes/linking functionalities

---
+++ src/Pages/HomePage/Home.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom"; 
+import logo from './logo.svg';
+import './Home.css';
+import TopNav from '../../Components/NavBar/Nav';
+import ProductCard from '../../Components/ProductCard/ProductCard';
+import Footer from '../../Components/Footer/Footer';
+import cardInfo from '../../Components/ProductCard/cardInfo';
+
+function Home() {
+    return (
+      <div className="App">
+        <TopNav />
+
+        <div className="product-cards">
+          {cardInfo.map(cardInfo => (
+            <ProductCard 
+              name={cardInfo.name}
+              image={cardInfo.image}
+              description={cardInfo.description}
+            />
+          ))}
+        </div>
+
+        <Footer />
+        
+      </div>
+    );
+  }
+  
+  export default Home;
+
+
+
