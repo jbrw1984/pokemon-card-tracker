7b18b1091ddebae859d9ed7179ddfd7fd5dc7b34
Avery Tribbett <apatribbett03@gmail.com>
2023-03-10 18:38:46 -0600

    Made footer responsive

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './FooterStyle.css';
 import pokeballLogo from './pokemon-symbol-logo-png-31 1.png';
@@ -8,8 +6,8 @@ import pokeballLogo from './pokemon-symbol-logo-png-31 1.png';
 class Footer extends React.Component {
     render() {
         return (
-            <Navbar id="footer-nav" expand="lg">
-                <Container id="footer-container">
+            <Navbar id="footer-nav" expand="md">
+                <div className='footer-div'>
                     <Navbar.Brand href="#home" >
                         <img 
                             alt="Pokeball PNG" 
@@ -20,11 +18,11 @@ class Footer extends React.Component {
                         />
                     </Navbar.Brand>
                     <Navbar.Collapse>
-                    <Navbar.Text>
-                        Pokemon Trading Cards. All original content herein is Copyright &copy;{new Date().getFullYear()}
-                    </Navbar.Text>
+                        <Navbar.Text className='footer-text'>
+                            Pokemon Trading Cards. All original content herein is Copyright &copy;{new Date().getFullYear()}
+                        </Navbar.Text>
                     </Navbar.Collapse>
-                </Container>
+                </div>
             </Navbar>
         )
     }
