9124a37ce870eef2d8cafc35b60fc67577bdb96d
Kobe Yang <kobeyang160@gmail.com>
2023-03-13 02:09:15 -0700

    made changes to css of cardRater

--- src/Components/CardRater/CardRater.tsx
+++ src/Components/CardRater/CardRater.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Col } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './CardRater.css';
@@ -6,23 +7,29 @@ import './CardRater.css';
 function CardRater() {
 
     return (
-        <Form>
-            <Form.Group controlId="cardRating">
-                <Form.Label htmlFor="">Rate the Card</Form.Label>
-                <Form.Text>The condition of the card on a scale of 1-10, with 10 being best.</Form.Text>
-                <Form.Range 
-                    id="cardRating"
+        <Form className="cardRaterForm">
+            <Form.Group className="cardRaterGroup" controlId="cardRating" >
+                <Col>
+                    <Form.Label className="cardRaterLabel" htmlFor="">Rate the Card</Form.Label>
+                </Col>
+                <Col>
+                    <Form.Text className="cardRaterText">The condition of the card on a scale of 1-10, with 10 being best.</Form.Text>
+                </Col>
+                <Col>
+                    <Form.Range 
+                        className="cardRaterSlider"
+                        id="cardRating"
 
-                    min={1}
-                    max={10}
-                    step={1}
-                    
-                />
+                        min={1}
+                        max={10}
+                        step={1}
+                        
+                    />
+                </Col>
             </Form.Group>
-            <Button variant="dark" type="submit" value="Submit">
+            <Button className="cardRaterButton" variant="dark" type="submit" value="Submit">
                 ENTER RATING
             </Button>
-
         </Form>
     );
   }
