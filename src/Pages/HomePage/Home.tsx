dd02742bc5b0cca929d3520eb35437664a9b8b58
Avery Tribbett <apatribbett03@gmail.com>
2023-03-21 09:35:45 -0500

    fixed linter warnings

--- src/Pages/HomePage/Home.tsx
+++ src/Pages/HomePage/Home.tsx
@@ -1,8 +1,6 @@
 import './Home.css';
 import TopNav from '../../Components/NavBar/Nav';
-import ProductCard from '../../Components/ProductCard/ProductCard';
 import Footer from '../../Components/Footer/Footer';
-import cardInfo from '../../Components/ProductCard/cardInfo';
 import DisplayCards from '../../Components/DisplayCards/DisplayCards';
 
 function Home() {
