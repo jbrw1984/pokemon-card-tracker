645639ea7c4e0ca93423773d1b628bc95194536a
Avery Tribbett <apatribbett03@gmail.com>
2023-05-17 16:47:18 -0500

    Got the index test mostly working still need to fix error with __dirname

--- api/src/database/index.ts
+++ api/src/database/index.ts
@@ -2,8 +2,8 @@ import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
 
 export const dbConnection = {
   url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  // Removed the two options here useNewUrlParser and useUnifiedTopology
+  // Mongoose 6 assumes these are true
   options: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
   },
 };
