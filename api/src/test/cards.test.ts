d28559c460981a4c20a0c98e1d87c83f5fcd1bb3
Avery Tribbett <apatribbett03@gmail.com>
2023-06-08 19:25:08 -0500

    Cleaned up test file

--- api/src/test/cards.test.ts
+++ api/src/test/cards.test.ts
@@ -22,34 +22,4 @@ describe('Testing Cards', () => {
       expect(result.body.data[0]._id).toHaveLength(24);
     });
   });
-  /*
-  describe('[GET] /users', () => {
-    it('response fineAll Users', async () => {
-      const usersRoute = new UserRoute();
-      const users = usersRoute.usersController.userService.users;
-
-      users.find = jest.fn().mockReturnValue([
-        {
-          _id: 'qpwoeiruty',
-          email: 'a@email.com',
-          password: await bcrypt.hash('q1w2e3r4!', 10),
-        },
-        {
-          _id: 'alskdjfhg',
-          email: 'b@email.com',
-          password: await bcrypt.hash('a1s2d3f4!', 10),
-        },
-        {
-          _id: 'zmxncbv',
-          email: 'c@email.com',
-          password: await bcrypt.hash('z1x2c3v4!', 10),
-        },
-      ]);
-
-      (mongoose as any).connect = jest.fn();
-      const app = new App([usersRoute]);
-      return request(app.getServer()).get(`${usersRoute.path}`).expect(200);
-    });
-  });
-  */
 });
