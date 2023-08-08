ba91ca57929647de7834252f95a87b7ec3d6ed1f
Kobe Yang <kobeyang160@gmail.com>
2023-08-08 01:30:02 -0700

    Added loading popup after search requests

--- app/src/Components/DisplayCards/DisplayCards.tsx
+++ app/src/Components/DisplayCards/DisplayCards.tsx
@@ -14,24 +14,34 @@ function DisplayCards ({ search, sortBy, order }: Props) {
   const [pageNumber, setPageNumber] = useState(0);
   const [cards, setCards] = useState<PokemonCard[]>([]);
   const [totalPages, setTotalPages] = useState(0);
+
+  const [isLoading, setIsLoading] = useState(false);
   
   // 12 Cards per page
-  const limit: number = 12;
+  const CARD_LIMIT: number = 12;
+  const SEARCH_DELAY: number = 300;
+
   
   // Update the cards each time the user changes the page
   useEffect(() => {
-    const fetchCurrentCards = async() => {
-      try {
-        const result = await fetch(`http://localhost:3000/cards/?page=${pageNumber + 1}&limit=${limit}&name=${search}&sort=${sortBy}&order=${order}`);
-        const data = await result.json();
-        setCards(data.data);
-        setTotalPages(data.totalPages);
-        console.log(search);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchCurrentCards();
+    setIsLoading(true);
+
+    const timerId = setTimeout(() => {
+      const fetchCurrentCards = async() => {
+        try {
+          const result = await fetch(`http://localhost:3000/cards/?page=${pageNumber + 1}&limit=${CARD_LIMIT}&name=${search}&sort=${sortBy}&order=${order}`);
+          const data = await result.json();
+          setCards(data.data);
+          setTotalPages(data.totalPages);
+          console.log(search);
+
+          setIsLoading(false);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchCurrentCards();
+    }, SEARCH_DELAY); 
   }, [pageNumber, search, sortBy, order]);
 
   // Map all of the current cards
@@ -47,7 +57,10 @@ function DisplayCards ({ search, sortBy, order }: Props) {
     setPageNumber(selected);
   };
 
+
+  
   return (
+
     <div className="page-container">
       <div className="pagination-container">
         <h4>Page {pageNumber + 1} of {totalPages}</h4>
@@ -61,11 +74,14 @@ function DisplayCards ({ search, sortBy, order }: Props) {
           breakClassName={"break-btn"}
         />
       </div>
+      {isLoading && <h1>Loading...</h1>}
       <div className="card-container">
         {displaySetOfCards}
       </div>
     </div>
   );
+
+  
 }
 
 export default DisplayCards;
